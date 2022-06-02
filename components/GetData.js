a91b38880136ac20ab7122d24e4aca0fb0b90620
Petr Kočí <petr.koci@samizdat.cz>
2022-06-02 13:02:31 +0200

    items

---
+++ components/GetData.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: 1,
+    name: "Andrej Babiš",
+    img: "./img/andrej-babis.jpg",
+  },
+  {
+    id: 1,
+    name: "Petr Pavel",
+    img: "./img/andrej-babis.jpg",
+  },
+];
+
+exporte default data;
