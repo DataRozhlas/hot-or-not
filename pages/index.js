d2827efb6ceda37c47c6a111011233c376cf2d8a
Petr Kočí <petr.koci@samizdat.cz>
2022-06-06 18:32:36 +0200

    pointer hover

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import MainPanel from "../components/MainPanel";
@@ -7,11 +7,26 @@ import data from "../components/GetData";
 
 export default function Home() {
   const [history, setHistory] = useState([]);
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const url = "https://data.irozhlas.cz/hot-or-not-results/prez-stats.json";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setResults(data);
+      });
+  }, []);
+
   return (
     <div className={styles.container}>
       <Header></Header>
       <MainPanel data={data} setHistory={setHistory}></MainPanel>
-      <HistoryPanel data={data} history={history}></HistoryPanel>
+      <HistoryPanel
+        data={data}
+        history={history}
+        results={results}
+      ></HistoryPanel>
     </div>
   );
 }
