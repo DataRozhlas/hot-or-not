c752266d4507b7414fb3743ae7079e8c3e134832
Petr Kočí <petr.koci@samizdat.cz>
2022-06-07 14:42:59 +0200

    deploy static site

--- out/_next/static/WQkk6nYFLh-h5CkS17xZ9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b787398bd6712e0b.css","static/chunks/pages/index-09415f577c1de450.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
