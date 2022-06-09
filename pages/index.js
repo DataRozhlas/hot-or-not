0f41ddafd5586a7f1f578ce7a02d513e1630e163
Petr Kočí <petr.koci@samizdat.cz>
2022-06-09 22:11:52 +0200

    added candidates

--- pages/index.js
+++ pages/index.js
@@ -3,46 +3,57 @@ import Link from "next/link";
 import Header from "../components/Header";
 import MainPanel from "../components/MainPanel";
 import HistoryPanel from "../components/HistoryPanel";
-import data from "../components/GetData";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [data, setData] = useState([]);
   const [history, setHistory] = useState([]);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
+    const url = "https://data.irozhlas.cz/hot-or-not-data/prez.json";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setData(data);
+      });
+  }, []);
+
+  useEffect(() => {
     const url = "https://data.irozhlas.cz/hot-or-not-results/prez-stats.json";
     fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         setResults(data);
       });
   }, []);
 
   return (
-    <div>
-      <Header text="Kdo by byl lepší prezident?"></Header>
-      <MainPanel data={data} setHistory={setHistory}></MainPanel>
-      <HistoryPanel
-        data={data}
-        history={history}
-        results={results}
-      ></HistoryPanel>
-      {history.length > 1 && (
-        <div className={styles.buttonContainer}>
-          <Link href="/vysledky">
-            <button className={styles.button}>Zobrazit žebříček</button>
-          </Link>
-        </div>
-      )}
-    </div>
+    data.length > 0 && (
+      <div>
+        <Header text="Kdo by byl lepší prezident?"></Header>
+        <MainPanel data={data} setHistory={setHistory}></MainPanel>
+        <HistoryPanel
+          data={data}
+          history={history}
+          results={results}
+        ></HistoryPanel>
+        {history.length > 1 && (
+          <div className={styles.buttonContainer}>
+            <Link href="/vysledky">
+              <button className={styles.button}>Zobrazit žebříček</button>
+            </Link>
+          </div>
+        )}
+      </div>
+    )
   );
 }
 
 export async function getStaticProps() {
   const data = await fetch(
     "https://data.irozhlas.cz/hot-or-not-data/prez.json"
-  ).then((res) => res.json());
+  ).then(res => res.json());
   return {
     props: {
       data: data,
